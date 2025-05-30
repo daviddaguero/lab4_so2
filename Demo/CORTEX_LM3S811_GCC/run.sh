3c13a0e6bba87c9db59ad33126fd8b56ab21ef1d
daviddaguero <david.aguero@mi.unc.edu.ar>
2025-05-30 15:38:30 -0400

    Removed unused comments in script and installation guide added

--- Demo/CORTEX_LM3S811_GCC/run.sh
+++ Demo/CORTEX_LM3S811_GCC/run.sh
@@ -1,41 +1,8 @@
 #!/bin/bash
 
-# cd ./Demo/CORTEX_LM3S811_GCC/
-
 make clean
 
 make
 
 echo "--- Launching QEMU ---"
 qemu-system-arm -machine lm3s811evb -kernel gcc/RTOSDemo.axf -serial stdio
-# qemu-system-arm -machine lm3s811evb -kernel gcc/RTOSDemo.axf -serial pty
-
-# # Creamos una tubería temporal para capturar la salida
-# PIPE=$(mktemp -u)
-# mkfifo "$PIPE"
-
-# # Lanzamos QEMU y redirigimos la salida a la pipe
-# qemu-system-arm \
-#   -machine lm3s811evb \
-#   -kernel gcc/RTOSDemo.axf \
-#   -serial pty > "$PIPE" 2>&1 &
-
-# # Esperamos a que QEMU imprima el PTY
-# PTY=""
-# while read line < "$PIPE"; do
-#   echo "$line"  # opcional: ver salida de QEMU
-#   if [[ "$line" =~ (/dev/pts/[0-9]+) ]]; then
-#     PTY="${BASH_REMATCH[1]}"
-#     break
-#   fi
-# done
-
-# rm "$PIPE"
-
-# # Conectamos con picocom si encontramos el PTY
-# if [[ -n "$PTY" ]]; then
-#   echo "Conectate con: picocom -b 19200 $PTY"
-#   picocom --echo --imap lfcrlf -b 19200 "$PTY"
-# else
-#   echo "No se pudo obtener el PTY 😓"
-# fi
